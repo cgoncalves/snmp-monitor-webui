e77cde5e9ee12c3f134586b2c81e2fc0042a7e2d
Carlos Goncalves <carlos.goncalves@ua.pt>
2011-11-17 14:59:08 +0000

    Change snmpget output format

--- plugins/snmp_linux_mem_swap.php
+++ plugins/snmp_linux_mem_swap.php
@@ -3,7 +3,7 @@
 <?php
 
   $oid = "1.3.6.1.4.1.2021.4.3.0";
-  $ret = shell_exec("snmpget -v 1 -c public $argv[1] $oid 2>&1");
+  $ret = shell_exec("snmpget -O en -v 1 -c public $argv[1] $oid 2>&1");
 
   if(!empty($ret))
   {
@@ -12,7 +12,7 @@
   }
 
   $oid = "1.3.6.1.4.1.2021.4.4.0";
-  $ret = shell_exec("snmpget -v 1 -c public $argv[1] $oid 2>&1");
+  $ret = shell_exec("snmpget -O en -v 1 -c public $argv[1] $oid 2>&1");
 
   if(!empty($ret))
   {
--- plugins/snmp_linux_storage_available.php
+++ plugins/snmp_linux_storage_available.php
@@ -3,7 +3,7 @@
 <?php
 
   $oid = "1.3.6.1.2.1.25.2.3.1.5.$argv[2]";
-  $ret = shell_exec("snmpget -v 1 -c public $argv[1] $oid 2>&1");
+  $ret = shell_exec("snmpget -O en -v 1 -c public $argv[1] $oid 2>&1");
 
   if(!empty($ret))
   {
@@ -12,7 +12,7 @@
   }
 
   $oid = "1.3.6.1.2.1.25.2.3.1.6.$argv[2]";
-  $ret = shell_exec("snmpget -v 1 -c public $argv[1] $oid 2>&1");
+  $ret = shell_exec("snmpget -O en -v 1 -c public $argv[1] $oid 2>&1");
 
   if(!empty($ret))
   {
@@ -21,7 +21,7 @@
   }
 
   $oid = "1.3.6.1.2.1.25.2.3.1.4.$argv[2]";
-  $ret = shell_exec("snmpget -v 1 -c public $argv[1] $oid 2>&1");
+  $ret = shell_exec("snmpget -O en -v 1 -c public $argv[1] $oid 2>&1");
 
   if(!empty($ret))
   {
