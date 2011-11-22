ff0606ed3afe602175f4b6a0031f12742974ef05
Carlos Goncalves <carlos.goncalves@ua.pt>
2011-11-22 21:15:38 +0000

    List available metrics when adding one

--- pages/add_metric.php
+++ pages/add_metric.php
@@ -1,6 +1,6 @@
 <?
+require_once('config.php');
 if (isset($_POST["submit"])) {
-	require_once('config.php');
 
 	$db_conn = mysql_connect($db_host, $db_user, $db_password);
 
@@ -11,8 +11,10 @@ if (isset($_POST["submit"])) {
 
 	mysql_select_db($db_name, $db_conn);
 
+	$parameters = $_POST['metric_file'] . ' ' . $_POST['metric_parameters'];
+
 	$sql="INSERT INTO metrics (Name, Parameters, DataType, Unit) VALUES
-		('$_POST[metric_name]', '$_POST[metric_parameters]', '$_POST[metric_datatype]', '$_POST[metric_unit]')";
+		('$_POST[metric_name]', '$parameters', '$_POST[metric_datatype]', '$_POST[metric_unit]')";
 
 	if (!mysql_query($sql,$db_conn)) {
 		die('Error: ' . mysql_error());
@@ -20,6 +22,16 @@ if (isset($_POST["submit"])) {
 
 	mysql_close($db_conn);
 	header("Location: index.php");
+}
+
+// List all available plugins
+$array_scripts = array();
+
+foreach (scandir($plugins_dir) as $filename)
+{
+	$file = $plugins_dir.'/'.$filename;
+	if (is_file($file))
+		array_push($array_scripts, $filename);
 }
 ?>
 
@@ -35,6 +47,17 @@ if (isset($_POST["submit"])) {
 				<input id="metric_name" name="metric_name" class="element text medium" type="text" maxlength="255" value=""/> 
 			</div> 
 		</li>
+		<li id="li_2" >
+			<label class="description" for="element_2">Metric</label>
+			<div>
+				<select name="metric_file">
+				<?php foreach ($array_scripts as $script) {
+						echo "<option value=$script>$script</option>";
+				} ?>
+				</select>
+			</div> 
+		</li>
+
 		<li id="li_2" >
 			<label class="description" for="element_2">Parameters</label>
 			<div>
